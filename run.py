e9a44f4d848ce91c9eb5223baee278f8713f360a
alexomaset <alexomaset711@gmail.com>
2019-04-08 10:16:09 +0300

    added the account info to ryn.py

---
+++ run.py
@@ -0,0 +1,14 @@
+from user import User
+from credential import Credential
+
+
+def create_account (account_name,user_name,user_password,confirmpassword):
+
+
+    """
+    function to create a new account
+    """
+
+    new_user = User(account_name,user_name,user_password,confirmpassword)
+
+    return new_user
