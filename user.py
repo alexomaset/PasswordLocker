6d73dca4736c10c7a9cfff13250099fda8ccf421
alexomaset <alexomaset711@gmail.com>
2019-04-08 09:57:25 +0300

    added user tests

---
+++ user.py
@@ -0,0 +1,63 @@
+import pyperclip
+class User:
+  """
+  class that generates new instances of user
+  """
+
+  user_detail = [] # Empty contact list 
+
+  def __init__(self,account_name,user_name,password , confirm_password):
+
+        self.account_name = account_name 
+        self.user_name = user_name
+        self.password = password
+        self.confirm_password = confirm_password
+
+
+
+  def save_detail(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        User.user_detail.append(self)
+
+  def delete_user(self):
+
+        '''
+        delete_user method deletes a saved contact from the contact_list
+        '''
+
+        User.user_detail.remove(self)
+
+  @classmethod
+  def find_by_username(cls,username):
+        '''
+        Method that takes in a username and returns a user that matches that username.
+       '''
+ 
+        for user in cls.user_detail:
+            if user.username == username:
+                return user
+
+
+  @classmethod
+  def display_all_details(cls):
+
+        return cls.user_detail             
+
+
+
+  @classmethod
+  def user_exist(cls,username):
+
+        for user in cls.user_detail:
+            if user.username == username:
+                    return True   
+
+
+  @classmethod
+  def copy_email(cls,username):
+        user_found = User.find_by_username(username)
+        pyperclip.copy(user_found.user)            
---
+++ user_test.py
@@ -0,0 +1,68 @@
+import pyperclip
+import unittest # Importing the unittest module
+from user import User # Importing the contact class
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("facebook", "amiredin" ,"123456", "123456") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.account_name,"facebook")
+        self.assertEqual(self.new_user.user_name,"amiredin")
+        self.assertEqual(self.new_user.password,"123456")
+        self.assertEqual(self.new_user.confirm_password,"123456")
+
+
+
+    def test_save_multiple_detail(self):
+
+        """
+        a method to check if we can save multiple user details to our details array
+        """
+
+        self.new_user.save_detail()
+        test_user = User("Test","user","123456","123456")
+        test_user.save_detail()
+        self.assertEqual(len(User.user_detail),3)
+
+    def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_detail()
+            test_user = User("Test" ,"user","0712345678","test@user.com") # new contact
+            test_user.save_detail()
+
+            self.new_user.delete_user()# Deleting a contact object
+            self.assertEqual(len(User.user_detail),1)
+
+
+
+    def test_display_all_details(self):
+
+        """
+        is a method that returns a list of all details saved
+        """
+
+        self.assertEqual(User.display_all_details(),User.user_detail)
+
+  
+
+
+
+if __name__ == '__main__':
+    unittest.main()
